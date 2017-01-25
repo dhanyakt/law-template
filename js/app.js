4e45ff699cc8b109562d3e83a4f9f924aa2e2114
Dhanya <dhanyakt@gmail.com>
2017-01-25 18:23:38 -0500

    feat: Added dropdown menu

---
+++ js/app.js
@@ -0,0 +1,19 @@
+$( document ).ready(function() {
+    $(".dropdown-button").dropdown();
+});
+
+      /*
+       * Open the drawer when the menu icon is clicked.
+       */
+    var menu = document.querySelector('#menu');
+    var main = document.querySelector('main');
+    var drawer = document.querySelector('#drawer');
+
+    menu.addEventListener('click', function(e) {
+        drawer.classList.toggle('open');
+        e.stopPropagation();
+    });
+    main.addEventListener('click', function() {
+        drawer.classList.remove('open');
+    });
+
